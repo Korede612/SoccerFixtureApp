08e25c77e09fd19143988461d2cc5cf78366f263
Oko-Osi Korede <okoosikorede@gmail.com>
2025-02-20 22:52:38 +0100

    Initial Commit

---
+++ SoccerFixtureApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SoccerFixtureApp
+//
+//  Created by Oko-Osi Korede on 20/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
